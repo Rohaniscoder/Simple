7f4eed3a57005afe9daa3c9df6e5d2ed81d0e87f
Rohaniscoder <109896047+Rohaniscoder@users.noreply.github.com>
2023-09-18 16:24:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -145,8 +145,8 @@ async def start(client, message):
                     reply_markup=InlineKeyboardMarkup(
                         [
                          [
-                          InlineKeyboardButton('ᴍᴜsᴛ ᴊᴏɪɴ ʙᴀᴄᴋᴜᴘ ᴄʜᴀɴɴᴇʟ ', url=f'https//t.me/Cinema_Moviesz')
-                  ]
+                          InlineKeyboardButton('ᴍᴜsᴛ ᴊᴏɪɴ ʙᴀᴄᴋᴜᴘ ᴄʜᴀɴɴᴇʟ ', url=f'https://t.me/Cinema_Moviesz')
+                         ]
                         ]
                     )
                 )
@@ -161,7 +161,7 @@ async def start(client, message):
                     reply_markup=InlineKeyboardMarkup(
                         [
                          [
-                          InlineKeyboardButton('ᴍᴜsᴛ ᴊᴏɪɴ ʙᴀᴄᴋᴜᴘ ᴄʜᴀɴɴᴇʟ ', url=f'https//t.me/Cinema_Moviesz')
+                          InlineKeyboardButton('ᴍᴜsᴛ ᴊᴏɪɴ ʙᴀᴄᴋᴜᴘ ᴄʜᴀɴɴᴇʟ ', url=f'https://t.me/Cinema_Moviesz')
                          ]
                         ]
                     )
@@ -229,7 +229,7 @@ async def start(client, message):
                 reply_markup=InlineKeyboardMarkup(
                     [
                      [
-                      InlineKeyboardButton('ᴍᴜsᴛ ᴊᴏɪɴ ʙᴀᴄᴋᴜᴘ ᴄʜᴀɴɴᴇʟ ', url=f'https//t.me/Cinema_Moviesz')
+                      InlineKeyboardButton('ᴍᴜsᴛ ᴊᴏɪɴ ʙᴀᴄᴋᴜᴘ ᴄʜᴀɴɴᴇʟ ', url=f'https://t.me/Cinema_Moviesz')
                      ]
                     ]
                 )
@@ -269,7 +269,7 @@ async def start(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
              [
-              InlineKeyboardButton('ᴍᴜsᴛ ᴊᴏɪɴ ʙᴀᴄᴋᴜᴘ ᴄʜᴀɴɴᴇʟ ', url=f'https//t.me/Cinema_Moviesz')
+              InlineKeyboardButton('ᴍᴜsᴛ ᴊᴏɪɴ ʙᴀᴄᴋᴜᴘ ᴄʜᴀɴɴᴇʟ ', url=f'https://t.me/Cinema_Moviesz')
              ]
             ]
         )
